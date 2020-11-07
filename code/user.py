244d67d2e0a54f334c8b0213074796b5e84421d5
Vedant Pareek <dunefro@gmail.com>
2020-11-07 10:12:23 +0000

    Update user.py for the sqlite to distribute find_by_username and find_by_id

--- code/user.py
+++ code/user.py
@@ -1,5 +1,43 @@
+import sqlite3
+
 class User(object):
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+    
+
+# This function accepts the username and find the row with the username value
+  @classmethod
+    def find_by_username(cls,username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        select_query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(select_query , (username ,))
+        row = result.fetchone()
+        # We need to return an User object
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
+    @classmethod
+    def find_by_id(cls,_id):
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        select_query = "SELECT * FROM users where id=?"
+        result = cursor.execute(select_query , (_id ,))
+        row = result.fetchone()
+        
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        connection.close()
+        return user
