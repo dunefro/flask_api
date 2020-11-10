3002e4a80199b04be878d7f31ee670cacf416a88
Vedant Pareek <dunefro@gmail.com>
2020-11-10 08:02:09 +0000

    Added get_all_items

--- code/sql_helper.py
+++ code/sql_helper.py
@@ -13,8 +13,10 @@ def _end_connection(connection):
 
 def create_table():
     connection , cursor = _create_connection()
-    create_table = "CREATE TABLE users (id INTEGER PRIMARY KEY, username text, password text)"
-    cursor.execute(create_table)
+    create_users_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+    cursor.execute(create_users_table)
+    create_items_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+    cursor.execute(create_items_table)
     _end_connection(connection)
 
 def create_user(username,password):
@@ -28,6 +30,57 @@ def check_for_user(username):
     check_user = "SELECT * FROM users WHERE username=?"
     result = cursor.execute(check_user,(username,))
     row = result.fetchone()
+    _end_connection(connection)
     if row:
         return True
     return False
+
+def check_for_item(name):
+    connection , cursor = _create_connection()
+    check_item = "SELECT * from items WHERE name=?"
+    result = cursor.execute(check_item,(name,))
+    row = result.fetchone()
+    _end_connection(connection)
+    if row:
+        return row[0] , row[1]
+    return None
+
+def create_item(name,price):
+    try:
+        connection , cursor = _create_connection()
+        create_item = "INSERT INTO items VALUES (?,?)"
+        cursor.execute(create_item,(name,price))
+        _end_connection(connection)
+        return True
+    except:
+        return False
+
+def get_all_items():
+    try:
+        connection , cursor = _create_connection()
+        get_all_items = "SELECT * FROM items"
+        result = cursor.execute(get_all_items)
+        items = []
+        while True:
+            item = result.fetchone()
+            if item:
+                items.append({'name': item[0],'price': item[1]})
+            else:
+                break
+        # items = [{'name': item[0]}]
+        # for item in result:
+            # items.append({'name': item[0],'price': item[1]})
+        return {'items': items}
+    except:
+        return None
+
+def delete_item(name):
+    try:
+        connection , cursor = _create_connection()
+        print('Item name is {}'.format(name))
+        delete_item = "DELETE FROM items WHERE name=?"
+        cursor.execute(delete_item , (name,))
+        _end_connection()
+        return True
+    except:
+        return False
