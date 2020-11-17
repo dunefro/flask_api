19b9ea2a0a9665b8d908b20fe6ed455b6c14ef3d
Vedant Pareek <dunefro@gmail.com>
2020-11-17 17:54:32 +0000

    Added Store and StoreList resource in restful

--- code/main.py
+++ code/main.py
@@ -5,6 +5,7 @@ from security import authenticate, identity
 from models import sql_helper
 from resources.user import UserRegister
 from resources.item import Item , ItemList
+from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
@@ -15,7 +16,13 @@ api = Api(app)
 app.secret_key = 'abcd'
 jwt = JWT(app , authenticate , identity)
 
-sql_helper.create_table()
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+# sql_helper.create_table()
 api.add_resource(Item , '/item/<string:name>')
 api.add_resource(ItemList , '/items')
 api.add_resource(UserRegister,'/register')
+api.add_resource(Store , '/store/<string:name>')
+api.add_resource(StoreList , '/stores')
